e6abd7efda262cb2f6246502a1b8c0440f93ac7a
segersniels <segers.n@hotmail.com>
2022-01-03 20:33:24 +0100

    :sparkles: Introduce shouldFetch

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,9 +1,18 @@
 import useSWR from 'swr';
 
-const useRequest = <T extends unknown>(url: string) => {
-  const fetcher = (url) => fetch(url).then((res) => res.json());
+interface Options {
+  shouldFetch?: boolean;
+}
 
-  const response = useSWR<T>(url, fetcher);
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+const useRequest = <T extends unknown>(
+  url: string,
+  options: Options = {
+    shouldFetch: true,
+  },
+) => {
+  const response = useSWR<T>(options.shouldFetch ? url : null, fetcher);
 
   return response;
 };
