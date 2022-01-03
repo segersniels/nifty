831a7917b639f92da28154196d8583d752d213cd
segersniels <segers.n@hotmail.com>
2022-01-03 20:36:52 +0100

    :bug: Remove ssr and client side mismatch between query params

--- src/pages/assets/index.tsx
+++ src/pages/assets/index.tsx
@@ -3,7 +3,6 @@ import Item from 'components/@pages/Assets/Item';
 import useRequest from 'hooks/useRequest';
 import { useRouter } from 'next/router';
 import React from 'react';
-import Skeleton from 'react-loading-skeleton';
 import Asset from 'types/Asset';
 
 import styles from './Assets.module.css';
@@ -18,15 +17,9 @@ const Assets = () => {
     `https://api.opensea.io/api/v1/assets?order_direction=asc&owner=${address}`,
   );
 
-  const user =
-    data?.assets[0].owner.user?.username ??
-    (address as string)?.slice(2, 8).toUpperCase();
-
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>
-        {user ? `${user}'s Assets` : <Skeleton width="30rem" />}
-      </h1>
+      <h1 className={styles.title}>Assets</h1>
 
       <div className={styles.wrapper}>
         {data?.assets.length
