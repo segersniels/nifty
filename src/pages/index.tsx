28c6f2ec7e20c8f0815c6e9622b2ed3a1e939619
segersniels <segers.n@hotmail.com>
2022-01-03 17:58:51 +0100

    :sparkles: Clean up landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,17 +11,21 @@ const Landing = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <input
-          onChange={(e) => setAddress(e.target.value)}
-          placeholder="address"
-          className={styles.input}
-        />
-        <button
-          className={styles.button}
-          onClick={() => router.push(`/assets?address=${address}`)}
-        >
-          Show
-        </button>
+        <h1 className={styles.title}>Nifty NFT Explorer</h1>
+
+        <div className={styles.search}>
+          <input
+            onChange={(e) => setAddress(e.target.value)}
+            placeholder="address"
+            className={styles.input}
+          />
+          <button
+            className={styles.button}
+            onClick={() => router.push(`/assets?address=${address}`)}
+          >
+            Show
+          </button>
+        </div>
       </div>
     </div>
   );
