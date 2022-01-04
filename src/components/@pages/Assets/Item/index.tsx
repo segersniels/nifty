1ea4d4d689ae52a2f56a3ba1c7a6b86b0a54b00a
segersniels <segers.n@hotmail.com>
2022-01-04 17:17:58 +0100

    :children_crossing: Don't show loading state when there is cached data

--- src/components/@pages/Assets/Item/index.tsx
+++ src/components/@pages/Assets/Item/index.tsx
@@ -42,7 +42,7 @@ const Item = (props: Props) => {
     return name;
   }, [asset?.collection.name, asset?.token_id, asset?.name]);
 
-  if (loading || isValidating) {
+  if (loading || (isValidating && !data)) {
     return <Loading />;
   }
 
