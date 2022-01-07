97500d2baa173034d7cce320c5ab08a06fdb35d2
segersniels <segers.n@hotmail.com>
2022-01-07 14:20:13 +0100

    :lipstick: Refactor cards to be less distracting and easier to look at

--- src/components/@pages/Assets/Item/index.tsx
+++ src/components/@pages/Assets/Item/index.tsx
@@ -54,61 +54,67 @@ const Item = (props: Props) => {
       target="_blank"
       rel="noreferrer"
     >
-      <p className={styles.price}>
-        Ξ{data?.collection?.stats.floor_price ?? 0}
-      </p>
+      <div className={styles.wrapper}>
+        <img className={styles.image} src={asset.image_url} />
 
-      <p className={styles.name}>{constructName()}</p>
+        <div className={styles.top}>
+          <p className={styles.price}>
+            Ξ{data?.collection?.stats.floor_price ?? 0}
+          </p>
 
-      <img className={styles.image} src={asset.image_url} />
+          <p className={styles.name}>{constructName()}</p>
+        </div>
 
-      <table className={styles.table}>
-        <tbody>
-          {asset.last_sale?.total_price && (
+        <table className={styles.table}>
+          <tbody>
+            {asset.last_sale?.total_price && (
+              <tr className={styles.row}>
+                <td className={styles.description}>Purchase Price</td>
+                <td className={styles.value}>
+                  Ξ
+                  {(asset.last_sale.total_price / 1000000000000000000).toFixed(
+                    3,
+                  )}
+                </td>
+              </tr>
+            )}
+
             <tr className={styles.row}>
-              <td className={styles.description}>Purchase Price</td>
+              <td className={styles.description}>Average Price</td>
               <td className={styles.value}>
-                Ξ
-                {(asset.last_sale.total_price / 1000000000000000000).toFixed(3)}
+                Ξ{data?.collection.stats.average_price.toFixed(3) ?? 0}
               </td>
             </tr>
-          )}
 
-          <tr className={styles.row}>
-            <td className={styles.description}>Average Price</td>
-            <td className={styles.value}>
-              Ξ{data?.collection.stats.average_price.toFixed(3) ?? 0}
-            </td>
-          </tr>
+            {!!data?.collection.stats.one_day_average_price && (
+              <tr className={styles.row}>
+                <td className={styles.description}>1d Average Price</td>
+                <td className={styles.value}>
+                  Ξ{data.collection.stats.one_day_average_price.toFixed(3)}
+                </td>
+              </tr>
+            )}
 
-          {!!data?.collection.stats.one_day_average_price && (
-            <tr className={styles.row}>
-              <td className={styles.description}>1d Average Price</td>
-              <td className={styles.value}>
-                Ξ{data.collection.stats.one_day_average_price.toFixed(3)}
-              </td>
-            </tr>
-          )}
+            {!!data?.collection.stats.seven_day_average_price && (
+              <tr className={styles.row}>
+                <td className={styles.description}>7d Average Price</td>
+                <td className={styles.value}>
+                  Ξ{data.collection.stats.seven_day_average_price.toFixed(3)}
+                </td>
+              </tr>
+            )}
 
-          {!!data?.collection.stats.seven_day_average_price && (
-            <tr className={styles.row}>
-              <td className={styles.description}>7d Average Price</td>
-              <td className={styles.value}>
-                Ξ{data.collection.stats.seven_day_average_price.toFixed(3)}
-              </td>
-            </tr>
-          )}
-
-          {!!data?.collection.stats.one_day_sales && (
-            <tr className={styles.row}>
-              <td className={styles.description}>Sales (24h)</td>
-              <td className={styles.value}>
-                {data.collection.stats.one_day_sales}
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+            {!!data?.collection.stats.one_day_sales && (
+              <tr className={styles.row}>
+                <td className={styles.description}>Sales (24h)</td>
+                <td className={styles.value}>
+                  {data.collection.stats.one_day_sales}
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </a>
   );
 };
