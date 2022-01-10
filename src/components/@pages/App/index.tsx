8ae91de4642392cc028e0c2bdbf1ba53cb614fbb
segersniels <segers.n@hotmail.com>
2022-01-10 10:42:40 +0100

    :chart_with_upwards_trend: Add google analytics

--- src/components/@pages/App/index.tsx
+++ src/components/@pages/App/index.tsx
@@ -1,7 +1,26 @@
+import Script from 'next/script';
 import React from 'react';
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script id="ga-analytics">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
