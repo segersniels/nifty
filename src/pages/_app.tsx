2fadf1ea2f75a925ef57270f3984ab62c04fe684
segersniels <segers.n@hotmail.com>
2022-01-21 20:30:02 +0100

    :recycle: Only disable SSR on landing page as that is the only page that needs to access window

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,51 @@ import 'styles/global.css';
 import 'styles/core.css';
 import 'react-loading-skeleton/dist/skeleton.css';
 
-import dynamic from 'next/dynamic';
+import * as gtag from 'lib/gtag';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import React, { useEffect } from 'react';
 
-const App = dynamic(() => import('@pages/App'), {
-  ssr: false,
-});
+const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,7 @@
-import Button from 'components/Button';
-import Layout from 'components/Layout';
-import Search from 'components/Search';
-import * as gtag from 'lib/gtag';
-import { useRouter } from 'next/router';
-import React, { useCallback } from 'react';
+import dynamic from 'next/dynamic';
 
-import styles from './Landing.module.css';
-
-const Landing = () => {
-  const router = useRouter();
-
-  const loginWithMetamask = useCallback(async () => {
-    try {
-      const accounts = await (window as any).ethereum.send(
-        'eth_requestAccounts',
-      );
-
-      gtag.event({
-        action: 'search_metamask',
-        category: 'address',
-        label: accounts.result[0],
-      });
-
-      router.push(`/assets?address=${accounts.result[0]}`);
-    } catch (error) {
-      return alert('Unable to authenticate with Metamask Wallet');
-    }
-  }, [router]);
-
-  return (
-    <Layout>
-      <div className={styles.container}>
-        <div className={styles.wrapper}>
-          <h1 className={styles.title}>Nifty NFT Explorer</h1>
-
-          <div className={styles.actions}>
-            <Search />
-
-            {!!(window as any).ethereum && (
-              <>
-                <p className={styles.divider}>or</p>
-                <Button onClick={loginWithMetamask}>Metamask</Button>
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
-};
+const Landing = dynamic(() => import('@pages/Landing'), {
+  ssr: false,
+});
 
 export default Landing;
