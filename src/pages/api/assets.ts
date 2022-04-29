664709500d6a28353c55b973c5b2176360575de5
segersniels <segers.n@hotmail.com>
2022-04-29 14:39:53 +0200

    :sparkles: Add ENS support

--- src/pages/api/assets.ts
+++ src/pages/api/assets.ts
@@ -1,8 +1,18 @@
+import { ethers } from 'ethers';
 import Asset from 'types/Asset';
 
 export default async function handler(req, res) {
+  let { owner } = req.query;
+
+  if (owner.endsWith('.eth')) {
+    const provider = new ethers.providers.CloudflareProvider();
+    owner = await provider.resolveName(owner);
+  }
+
   const response = await fetch(
-    `https://api.opensea.io/api/v1/assets?limit=${req.query.limit}&offset=${req.query.offset}&owner=${req.query.owner}`,
+    `https://api.opensea.io/api/v1/assets?limit=${req.query.limit}&offset=${
+      req.query.offset
+    }&owner=${owner.toLowerCase()}`,
     {
       headers: {
         'X-API-KEY': process.env.OPENSEA_API_KEY,
