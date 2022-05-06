689703c066e2716a90533d70fc0da576d6d0a698
segersniels <segers.n@hotmail.com>
2022-05-06 17:37:02 +0200

    :sparkles: Display latest ETH price in USD in footer

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,3 +1,4 @@
+import { useValueContext } from 'context/ValueContext';
 import React from 'react';
 
 import styles from './Layout.module.css';
@@ -7,11 +8,14 @@ interface Props {
 }
 
 const Layout = (props: Props) => {
+  const { price } = useValueContext();
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>{props.children}</div>
 
       <div className={styles.footer}>
+        <p className={styles.price}>{price}$</p>
         <p>0x73Da490563C0ccBFd1b86763ccB11B4DaBbCB550</p>
       </div>
     </div>
