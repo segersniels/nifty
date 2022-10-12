1ca8dbe376e992461ee11ca99d8eae73c9eeeee1
segersniels <segers.n@hotmail.com>
2022-10-12 10:16:30 +0200

    :alien: Apparently opensea switched where they store their images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   swcMinify: true,
   images: {
-    domains: ['lh3.googleusercontent.com', 'openseauserdata.com'],
+    domains: ['lh3.googleusercontent.com', 'openseauserdata.com', 'i.seadn.io'],
     formats: ['image/avif', 'image/webp'],
   },
   experimental: {
